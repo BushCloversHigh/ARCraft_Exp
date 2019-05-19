45d9ec7852417503bf66d5af45e444676f1fd05a
BushCloversHigh <32239432+BushCloversHigh@users.noreply.github.com>
2019-05-20 02:32:23 +0900

    拡大、縮小を実装

--- Assets/Gitogito/Scripts/CraftController.cs
+++ Assets/Gitogito/Scripts/CraftController.cs
@@ -9,7 +9,11 @@ public class CraftController : MonoBehaviour
     [SerializeField] private GameObject blocksContent;
     private BlockElement[] blockElements;
 
+    [SerializeField] private Transform putedBlocks;
+
     private Transform mainCam;
+
+    private bool firstPut = false;
 
     private void Awake ()
     {
@@ -32,25 +36,45 @@ public class CraftController : MonoBehaviour
         }
     }
 
-    public void PutBlock ()
+    public void PutBlock (Vector3 touchPos)
     {
-        Touch touch = Input.GetTouch (0);
-        Ray camRay = Camera.main.ScreenPointToRay (touch.position);
+        Ray camRay = Camera.main.ScreenPointToRay (touchPos);
         RaycastHit hit;
 
         if(Physics.Raycast(camRay, out hit))
         {
-            Vector3 putPos = hit.point;
-            putPos.x = FloatToInt (putPos.x);
-            putPos.y = FloatToInt (putPos.y);
-            putPos.z = FloatToInt (putPos.z);
-
-            Instantiate (currentBlock, putPos, Quaternion.Euler (Vector3.zero));
+            if (!firstPut)
+            {
+                firstPut = true;
+                putedBlocks.transform.position = hit.point;
+            }
+            GameObject go_block = Instantiate (currentBlock, putedBlocks);
+            go_block.transform.eulerAngles = Vector3.zero;
+            go_block.transform.localScale = Vector3.one + Vector3.one * 0.001f;
+            go_block.transform.position = hit.point;
+            Vector3 localPos = go_block.transform.localPosition;
+            localPos = new Vector3 (RoundToInt (localPos.x), FloorToInt (localPos.y) + 0.5f, RoundToInt (localPos.z));
+            go_block.transform.localPosition = localPos;
         }
     }
 
-    private float FloatToInt (float f)
+    public void ChangeScale (float scaleDelta)
+    {
+        Vector3 scale = putedBlocks.localScale + Vector3.one * scaleDelta * 0.01f;
+        scale.x = Mathf.Clamp (scale.x, 0.1f, 1f);
+        scale.y = Mathf.Clamp (scale.y, 0.1f, 1f);
+        scale.z = Mathf.Clamp (scale.z, 0.1f, 1f);
+
+        putedBlocks.localScale = scale;
+    }
+
+    private float RoundToInt (float f)
     {
         return Mathf.Round (f);
+    }
+
+    private float FloorToInt (float f)
+    {
+        return Mathf.Floor (f);
     }
 }
---
+++ Assets/Gitogito/Scripts/TouchPanel.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TouchPanel : MonoBehaviour, IPointerDownHandler
+{
+
+    private bool isTouching = false, isDouble = false;
+
+    [SerializeField] private CraftController craftController;
+
+    private float scale1, scale2;
+
+    private void Update ()
+    {
+        if(Input.touchCount == 0)
+        {
+            isTouching = false;
+            isDouble = false;
+            return;
+        }
+
+        if (!isTouching)
+        {
+            return;
+        }
+
+        if (Input.touchCount == 1)
+        {
+            if (isDouble)
+            {
+                return;
+            }
+
+            Touch touch = Input.GetTouch (0);
+            if (touch.phase == TouchPhase.Ended)
+            {
+                craftController.PutBlock (touch.position);
+            }
+        }
+        else if (Input.touchCount == 2)
+        {
+            Touch touch1 = Input.GetTouch (0);
+            Touch touch2 = Input.GetTouch (1);
+            scale1 = Vector2.Distance (touch1.position, touch2.position);
+            if (!isDouble)
+            {
+                isDouble = true;
+                scale2 = scale1;
+            }
+            craftController.ChangeScale (scale1 - scale2);
+            scale2 = scale1;
+        }
+    }
+
+    private bool FloatTermZero (float f)
+    {
+        return System.Math.Abs (f) > Mathf.Epsilon;
+    }
+
+    public void OnPointerDown (PointerEventData eventData)
+    {
+        isTouching = true;
+    }
+
+}
